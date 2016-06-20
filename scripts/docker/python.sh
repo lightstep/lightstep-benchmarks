78c100c70114eda56f802f5654068f6c2017adb5
Josh MacDonald <josh.macdonald@gmail.com>
2016-06-20 10:57:00 -0700

    Python loadtest

---
+++ scripts/docker/python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO pyclient moves to /github.com/lightstep/lightstep-tracer-python/benchmark
+
+ln ${GOPATH}/src/pyclient.py ${DBUILD}/pyclient.py 
+ln ${GOPATH}/src/requirements.txt ${DBUILD}/requirements.txt
