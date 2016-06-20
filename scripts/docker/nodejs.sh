a830bc24387b498a25a51121bc59d5eefd8f268c
Josh MacDonald <josh.macdonald@gmail.com>
2016-06-20 11:01:34 -0700

    Support building w/ 'npm link' in use

--- scripts/docker/nodejs.sh
+++ scripts/docker/nodejs.sh
@@ -5,6 +5,9 @@
 
 mkdir -p ${DBUILD}/node_modules
 
-(cd ${GOPATH} && tar -L -C src -cf - node_modules) | (cd ${DBUILD}/node_modules && tar -xf -)
+# TODO the -L here is undesireable b/c it copies 4x Docker context,
+# however this allows use of "npm link" local development.
+(cd ${GOPATH} && tar -L -C src -cf - node_modules) | \
+    (cd ${DBUILD}/node_modules && tar -xf -)
 ln ${GOPATH}/src/package.json ${DBUILD}
 ln ${GOPATH}/src/jsclient.js ${DBUILD}
