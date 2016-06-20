e851a0e9e74101068bfa43bf64fe179ab719cb0c
Josh MacDonald <josh.macdonald@gmail.com>
2016-06-20 11:00:14 -0700

    Partial Ruby benchmark updates

---
+++ scripts/docker/ruby.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO rbclient moves to /github.com/lightstep/lightstep-tracer-ruby/benchmark
+
+ln ${GOPATH}/src/rbclient.rb ${DBUILD}/rbclient.rb
+ln ${GOPATH}/src/Gemfile ${DBUILD}/Gemfile
