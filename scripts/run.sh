fdc21d88a6dddf70b7197c10af9253519044d962
Josh MacDonald <josh.macdonald@gmail.com>
2016-07-07 16:46:10 -0700

    Update run.sh for config; introduce single-threaded config

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-./benchmark.sh python 1
-./benchmark.sh python 2
-./benchmark.sh nodejs 1
-./benchmark.sh nodejs 2
-./benchmark.sh golang 1
-./benchmark.sh golang1 2
+## No logging benchmarks
 
+# Concurrency=1
+./benchmark.sh python 1 single-thread-zero-logs
+./benchmark.sh python 2 single-thread-zero-logs
+./benchmark.sh nodejs 1 single-thread-zero-logs
+./benchmark.sh nodejs 2 single-thread-zero-logs
+./benchmark.sh golang 1 single-thread-zero-logs
+./benchmark.sh golang1 2 single-thread-zero-logs
+
+# Concurrency=2
+./benchmark.sh golang 2 two-threads-zero-logs
+./benchmark.sh golang 4 two-threads-zero-logs
