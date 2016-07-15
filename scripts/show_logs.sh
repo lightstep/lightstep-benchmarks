0bf5febd0f0e74ddf076342261dd0ae50e273615
Josh MacDonald <josh.macdonald@gmail.com>
2016-07-15 16:38:47 -0700

    Update show_logs.sh

--- scripts/show_logs.sh
+++ scripts/show_logs.sh
@@ -6,8 +6,10 @@ TITLE=${1}
 CLIENT=${2}
 CPUS=${3}
 TEST_CONFIG_BASE=${4}
+CLOUD_ZONE="us-central1-a"
+PROJECT_ID="lightstep-dev"
+GCLOUD=gcloud
+SSH="${GCLOUD} compute ssh --project ${PROJECT_ID} --zone ${CLOUD_ZONE}"
 MACHINE=bench-${TITLE}-${CLIENT}-${CPUS}-${TEST_CONFIG_BASE}
 
-eval $(docker-machine env ${MACHINE})
-
-docker logs ${MACHINE} 2>&1
+${SSH} ${MACHINE} -- sudo docker logs ${MACHINE} 2>&1
