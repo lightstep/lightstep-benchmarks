4994c6c493224512bf699e99ab4d010afd8d7bfa
Josh MacDonald <josh.macdonald@gmail.com>
2017-01-11 11:20:03 -0800

    Ruby build fix

--- scripts/docker/ruby.sh
+++ scripts/docker/ruby.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+(cd ${CLIENTS_DIR}/lightstep-tracer-ruby && rm pkg/lightstep-*.gem)
 (cd ${CLIENTS_DIR}/lightstep-tracer-ruby && rake build && cp pkg/lightstep-*.gem ${DBUILD}/lightstep.gem)
 (cd ${CLIENTS_DIR}/lightstep-tracer-ruby && cp benchmark.rb ${DBUILD})
