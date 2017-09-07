97fdd27dcce60b10be096ddc3ba0fca3e6798f53
Jack Beasley <jack.beasley@gmail.com>
2017-09-07 14:27:15 -0700

    Better testing

--- clientlib/client_controller_test.go
+++ clientlib/client_controller_test.go
@@ -3,55 +3,74 @@ package clientlib
 import (
 	"github.com/lightstep/lightstep-benchmarks/benchlib"
 
-	"fmt"
 	"testing"
 )
 
+type TestClientControllerTest struct {
+	Control          benchlib.Control
+	ExpectedResponse *benchlib.Result
+	ExpectedError    error
+}
+
+var TestClients = []TestClient{
+	CreateProcessClient([]string{"./goclient"}),
+}
+
+var TestRuns = []TestClientControllerTest{
+	TestClientControllerTest{
+		Control: benchlib.Control{
+			Concurrent:    1,
+			Work:          1000,
+			Repeat:        10,
+			Trace:         false,
+			Sleep:         1,
+			SleepInterval: 5,
+		},
+		ExpectedResponse: &benchlib.Result{},
+		ExpectedError:    nil,
+	},
+	TestClientControllerTest{
+		Control: benchlib.Control{
+			Concurrent:    1,
+			Work:          1000,
+			Repeat:        10,
+			Trace:         true,
+			Sleep:         10,
+			SleepInterval: 100,
+		},
+		ExpectedResponse: &benchlib.Result{},
+		ExpectedError:    nil,
+	},
+}
+
 func TestClientStart(t *testing.T) {
 	controller := CreateHTTPTestClientController()
-	fmt.Printf("Starting Control server")
 	controller.StartControlServer()
-	fmt.Printf("Control server started")
 
-	client := CreateProcessClient([]string{"./goclient"})
-	err := controller.StartClient(client)
-	if err != nil {
-		t.Error("Should not have errored")
-	}
+	for _, client := range TestClients {
+		err := controller.StartClient(client)
+		if err != nil {
+			t.Error("Client %v failed to start with error: %v", client, err)
+		}
 
-	// Run warmup tasks
-	res, err := controller.Run(benchlib.Control{
-		Concurrent:    1,
-		Work:          1000,
-		Repeat:        10,
-		Trace:         false,
-		Sleep:         1,
-		SleepInterval: 5,
-	})
-	if err != nil {
-		t.Error("Should not have errored")
-	}
-	fmt.Println(res)
-	if res == nil {
-		t.Error("Should have returned a result")
-	}
+		for _, test := range TestRuns {
+			// Run warmup tasks
+			res, err := controller.Run(test.Control)
+			if test.ExpectedError == nil && err != nil {
+				t.Error("Run with control: %v unexpectedly returned an error: %v", test.Control, err)
+			}
 
-	res, err = controller.Run(benchlib.Control{
-		Concurrent:    1,
-		Work:          1000,
-		Repeat:        10,
-		Trace:         true,
-		Sleep:         10,
-		SleepInterval: 100,
-	})
+			if test.ExpectedError != nil && err == nil {
+				t.Error("Run with control: %v expected error: %v, but returned no error", test.Control, test.ExpectedError)
+			}
 
-	if err != nil {
-		t.Error("Should not have errored")
-	}
-	fmt.Println(res)
-	if res == nil {
-		t.Error("Should have returned a result")
-	}
+			if test.ExpectedResponse != nil && res == nil {
+				t.Error("Run with control: %v should have returned a result, but did not", test.Control)
+			}
 
-	controller.StopClient()
+		}
+
+		controller.StopClient()
+
+	}
 }
