dfff8abbbb3a6a509955ca51bdcc44b2717b2578
Isaac Zinda <isaac@lightstep.com>
2019-07-25 13:22:16 -0700

    add cpp bindings test

--- test.py
+++ test.py
@@ -27,23 +27,25 @@ class TestVanilla:
 
 ## CPP TESTS ##
 
-# @pytest.fixture(scope="module")
-# def cpp_client():
-#     with Controller(['python3', 'clients/python_client.py', '8360', 'cpp'],
-#             client_name='cpp_python_client',
-#             num_satellites=8) as c:
-#         yield c
-#
-# def test_vanilla_client(cpp_client):
-#     result = vanilla_controller.benchmark(
-#         trace=True,
-#         with_satellites=True,
-#         spans_per_second=100,
-#         runtime=5)
-#
-#     assert isinstance(result, Result)
-#     assert result.spans_sent > 0
-#     assert result.dropped_spans == 0
+class TestSidecar:
+    @pytest.fixture(scope='class')
+    def client(self):
+        with Controller(['python3', 'clients/python_client.py', '8360', 'cpp'],
+                client_name='cpp_python_client',
+                num_satellites=8) as c:
+            yield c
+
+    def test_send_spans(self, client):
+        result = client.benchmark(
+            trace=True,
+            with_satellites=True,
+            spans_per_second=100,
+            runtime=5)
+
+        assert isinstance(result, Result)
+        assert result.spans_sent > 0
+        assert result.dropped_spans == 0
+
 
 ## VANILLA TESTS WITH SIDECAR ##
 class TestSidecar:
