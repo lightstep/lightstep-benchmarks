9026a3644db23406ff7cf3baad7217052623d379
Isaac Zinda <isaac@lightstep.com>
2019-08-08 11:36:08 -0700

    print message about which client we are testing

--- conftest.py
+++ conftest.py
@@ -6,5 +6,11 @@ def pytest_generate_tests(metafunc):
     # This is called for every test. Only get/set command line arguments
     # if the argument is specified in the list of test "fixturenames".
     client_name = metafunc.config.option.client_name
+
+    if client_name:
+        print(f'client_name = {client_name}')
+    else:
+        print("client_name argument was not passed to pytest.")
+
     if 'client_name' in metafunc.fixturenames and client_name is not None:
         metafunc.parametrize("client_name", [client_name])
