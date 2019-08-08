4bbae6bc1a0a12eeaa018fe2581f618a06fdaca7
Isaac Zinda <isaac@lightstep.com>
2019-08-08 11:04:02 -0700

    fix typo

--- disconnect_graphs.py
+++ disconnect_graphs.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from benckmark.controller import Controller
+from benchmark.controller import Controller
 from benchmark.satellite import MockSatelliteGroup as SatelliteGroup
 import numpy as np
 import argparse
