ce9c6c9387e6a282b3cadf5f96b7171a6fc5cd60
Isaac Zinda <isaac@lightstep.com>
2019-08-30 10:45:13 -0700

    handle psutil.NoSuchProcess

--- benchmark/controller.py
+++ benchmark/controller.py
@@ -162,7 +162,7 @@ class ClientProcess(subprocess.Popen):
                     self._clock_time = time.time() - start_clock_time
             # if the child process has shut down, ignore because self.poll()
             # will detect this soon
-            except psutil.AccessDenied:
+            except (psutil.AccessDenied, psutil.NoSuchProcess):
                 pass
 
     def _save_list_stats(self, cpu_list, memory_list):
@@ -190,7 +190,7 @@ class ClientProcess(subprocess.Popen):
                     memory_list.append(memory_usage)
             # if the child process has shut down, ignore because self.poll()
             # will detect this soon
-            except psutil.AccessDenied:
+            except (psutil.AccessDenied, psutil.NoSuchProcess):
                 pass
 
     def get_results(self):
